bad20c93e6e9fecd6a3abd63fc4f6772fbda7409
AJ Guiles <ashiguiles@gmail.com>
2019-04-21 19:21:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,36 @@
-class Student 
+# class Student < User
+  
+#   attr_accessor :knowlege
+  
+#   def initialize
+    
+#   @knowlege = []
+  
+#   end
+  
+#   def learn(string)
+#     @knowledge << string
+#   end
 
+# def knowledge
+#   @knowledge
+# end
+
+# end
+
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
